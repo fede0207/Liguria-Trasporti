32b4d86fb031407c8eb8903ea99a74249a54aab4
fede0207 <federicofini96@gmail.com>
2026-08-17 19:06:24 +0200

    FirebaseId Migration and docker compose update

---
+++ Migrations/20260817105301_FirebaseId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Liguria_Trasporti.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirebaseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FirebaseId",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirebaseId",
+                table: "Employees");
+        }
+    }
+}
---
+++ Migrations/20260817110110_FirebaseIdToEmployee.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Liguria_Trasporti.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirebaseIdToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
